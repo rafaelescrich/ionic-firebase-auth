5be40bc56383b159ea9dc50bc89e614e75c29245
Jorge Vergara <javebratt@gmail.com>
2016-03-17 21:50:40 -0500

    Signup Adding the signup form and controller

---
+++ www/app/auth/signup/signupCtrl.js
@@ -0,0 +1,20 @@
+(function(){
+	'use strict';
+
+	angular
+			.module('app.auth')
+			.controller('SignupCtrl', SignupCtrl);
+
+	SignupCtrl.$inject = ['$scope', '$state', 'AuthService'];
+	function SignupCtrl($scope, $state, AuthService){
+		$scope.data = {};
+
+		$scope.createUser = function(){
+				var newEmail = $scope.data.email;
+				var newPassword = $scope.data.password;
+				var newFullName = $scope.data.fullName;
+				var selectedPlan = $state.params.pId;
+				AuthService.signupEmail(newEmail, newPassword, newFullName, selectedPlan);
+		};
+	}
+})();
