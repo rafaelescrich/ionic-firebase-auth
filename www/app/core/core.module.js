52cbfa3c9be92241b7dd34ba8ff85ea88d10fe35
Jorge Vergara <javebratt@gmail.com>
2016-03-17 20:31:46 -0500

    Core Module Creates the core module that will handle core dependencies Creates the routes for the auth flow

---
+++ www/app/core/core.module.js
@@ -0,0 +1,25 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('app.core', [
+            'ionic',
+            'firebase',
+    ]);
+
+    angular
+    	.module('app.core')
+    	.run(['$ionicPlatform',
+    	     function($ionicPlatform) {
+    	     	$ionicPlatform.ready(function() {
+					    if(window.cordova && window.cordova.plugins.Keyboard) {
+					      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+					      cordova.plugins.Keyboard.disableScroll(true);
+					    }
+					    if(window.StatusBar) {
+					      StatusBar.styleDefault();
+					    }
+					  });
+			}])
+			.constant('FBURL', 'your-firebase-url.firebaseio.com/')
+})();
---
+++ www/app/core/routes.js
@@ -0,0 +1,29 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('app.core')
+    .config(['$stateProvider', '$urlRouterProvider', '$ionicConfigProvider',
+        function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+        	$stateProvider
+	        	.state('login', {
+	        		url: '/login',
+	        		templateUrl: 'app/auth/login/login.html',
+	        		controller: 'LoginCtrl',
+	        	})
+
+	        	.state('signup', {
+	        		url: '/signup/:pId',
+	        		templateUrl: 'app/auth/signup/signup.html',
+	        		controller: 'SignupCtrl',
+	        	})
+
+	        	.state('passwordResetForm', {
+	        		url: '/passwordResetForm',
+	        		templateUrl: 'app/auth/login/passwordResetForm.html',
+	        		controller: 'PasswordResetCtrl',
+	        	});
+	        $urlRouterProvider.otherwise('dashboard');
+    }]);
+})();
