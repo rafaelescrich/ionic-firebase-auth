318dc25a651ec5a643b298ff2cd8afb343ffe365
Jorge Vergara <javebratt@gmail.com>
2016-03-22 15:12:22 -0500

    Adds form validation to signup module

--- app/auth/signup/signupCtrl.js
+++ app/auth/signup/signupCtrl.js
@@ -9,12 +9,15 @@
 	function SignupCtrl($scope, $state, AuthService){
 		$scope.data = {};
 
-		$scope.createUser = function(){
+		$scope.createUser = function(signupForm){
+			if (signupForm.$valid) {
 				var newEmail = $scope.data.email;
 				var newPassword = $scope.data.password;
 				var newFullName = $scope.data.fullName;
 				var selectedPlan = $state.params.pId;
 				AuthService.signupEmail(newEmail, newPassword, newFullName, selectedPlan);
+			};
+
 		};
 	}
 })();
